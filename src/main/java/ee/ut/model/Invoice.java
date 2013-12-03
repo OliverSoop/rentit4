c0c3389a3a22053bd5407152d8b46dea277d5c43
OliverSoop <oliversoop@gmail.com>
2013-12-03 21:31:00 +0200

    GitIgnore improved Reminders start

--- src/main/java/ee/ut/model/Invoice.java
+++ src/main/java/ee/ut/model/Invoice.java
@@ -1,4 +1,10 @@
 package ee.ut.model;
+import java.util.Date;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
